43adb2243590bdd3fb73df05a4e0cc39fc99a7a9
BatoBudaev <Axl206311>
2023-03-17 22:51:38 +0700

    fix

--- src/test/java/mantis/tests/IssueTest.java
+++ src/test/java/mantis/tests/IssueTest.java
@@ -3,13 +3,19 @@ package mantis.tests;
 import mantis.pages.MantisSite;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class IssueTest extends BaseTest {
     private MantisSite mantisSite;
 
     @Test
     public void addAndRemoveIssueTest() throws InterruptedException {
+        String descriptionText = "Some description";
+        String summaryText = "Some summary";
+
         mantisSite = new MantisSite(driver);
         mantisSite.login("admin", "admin20");
         Thread.sleep(1000);
@@ -19,19 +25,26 @@ public class IssueTest extends BaseTest {
         Thread.sleep(1000);
 
         softAssert.assertTrue(driver.findElement(By.cssSelector(".widget-title")).isDisplayed());
-        String descriptionText = "Some description";
-        mantisSite.getReportIssuePage().enterIssueFields("Some summary", descriptionText);
+        mantisSite.getReportIssuePage().enterIssueFields(summaryText, descriptionText);
         Thread.sleep(1000);
 
         softAssert.assertTrue(driver.findElement(By.xpath("//p[text()='Operation successful.']")).isDisplayed());
         mantisSite.getReportIssuePage().clickViewIssueButton();
         Thread.sleep(1000);
 
+        String bugId = driver.findElement(By.cssSelector("td.bug-id")).getText();
         softAssert.assertEquals(descriptionText, driver.findElement(By.cssSelector(".bug-description:last-child")).getText());
+        softAssert.assertTrue(driver.findElement(By.cssSelector(".bug-summary:last-child")).getText().contains(summaryText));
         mantisSite.getViewIssuePage().clickDeleteButton();
         Thread.sleep(1000);
 
         mantisSite.getViewIssuePage().clickDeleteIssuesButton();
+
+        List<WebElement> issueList = driver.findElements(By.cssSelector("td.column-id"));
+        for (WebElement e : issueList) {
+            softAssert.assertFalse(bugId.equals(e.getText()));
+        }
+
         softAssert.assertAll();
         Thread.sleep(1000);
     }
